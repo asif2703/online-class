2858eb561c49f8e76fa10cd58ff47a19bd73dc9c
asif2703/ git config --global user.name asif2703 <wapasif@gmail.com>
2020-08-27 23:41:18 +0600

    start online class

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/online-class.jpg';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hello Online Class</h1>
+            <img src={logo} alt=""/>
+        </div>
+    );
+};
+
+export default Header;
